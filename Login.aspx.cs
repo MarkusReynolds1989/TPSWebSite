9bdd736700ba363dd1c405eae7898418ec475125
marsaray <41028417+marsaray@users.noreply.github.com>
2019-04-03 09:48:48 -0500

    Moving Staffrequest code mistake

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -11,22 +11,4 @@ public partial class Login : System.Web.UI.Page
     {
 
     }
-    //when they press the select button we update the staffrequest for staff to be added
-    protected void OnRowEvent_SelectRow(object sender, EventArgs e)
-    {
-        //Eddie, check this out and let me know what you think
-        //We need to get the information from the currently selected row and pass it to
-        //The staffrequest table to build it, we can use this as a base and hack it
-        int StaffID1;
-        int StaffID2;
-        int StaffID3;
-        //Get the currently selected row using the SelectedRow property.
-        GridViewRow row = dgCustomer.SelectedRow;
-        // And you respective cell's value
-        StaffID1 = row.Cells[1].Text;
-    }
-    protected void OnButtonClick_btnAddStaff(object sender, EventArgs e)
-    {
-        AddStaffRequest(StaffID1,StaffID2,StaffID3);
-    }
 }
