a82859f18c120de7af8314be7d42dd4dd92a1418
Markus Reynolds <marsatreus@gmail.com>
2019-04-02 20:05:38 -0700

    Added a method for IP4 data security to clsDataLayer

--- App_Code/clsDataLayer.cs
+++ App_Code/clsDataLayer.cs
@@ -248,6 +248,36 @@ namespace TPS.App_Code
             sqlDA.Fill(DS.tblStaffMember);
             return DS;
         }
+
+        /*IP4 For Security
+        ///////////////////
+        //////////////////*/
+        public static string GetIP4Address()
+        {
+            string IP4Address = string.Empty;
+            foreach (IPAddress IPA in Dns.GetHostAddresses(HttpContext.Current.Request.UserHostAddress))
+            {
+                if (IPA.AddressFamily.ToString() == "InterNetwork")
+                {
+                    IP4Address = IPA.ToString();
+                    break;
+                }
+            }
+            if (IP4Address != string.Empty)
+            {
+                return IP4Address;
+            }
+            foreach (IPAddress IPA in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                if (IPA.AddressFamily.ToString() == "InterNetwork")
+                {
+                    IP4Address = IPA.ToString();
+                }
+                break;
+            }
+            return IP4Address;
+        }
+
     }
 }
 
