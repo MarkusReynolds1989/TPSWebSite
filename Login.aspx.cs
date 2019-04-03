8541ae7e89ed059163c0b50d305604631987193d
marsaray <41028417+marsaray@users.noreply.github.com>
2019-04-03 09:24:44 -0500

    Eddie, please see login.aspx.cs

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -11,4 +11,14 @@ public partial class Login : System.Web.UI.Page
     {
 
     }
+    protected void OnButtonClick_btnAddStaff(object sender, EventArgs e)
+    {
+        //Eddie, check this out and let me know what you think
+        //We need to get the information from the currently selected row and pass it to
+        //The staffrequest table to build it, we can use this as a base and hack it
+        //Get the currently selected row using the SelectedRow property.
+        GridViewRow row = dgCustomer.SelectedRow;
+
+        // And you respective cell's value
+        TextBox1.Text = row.Cells[1].Text
 }
