fd40baa4d1d6c441afe4b736c045bf5b25caad99
marsaray <41028417+marsaray@users.noreply.github.com>
2019-04-03 09:49:14 -0500

    Eddie please see staffrequest.aspx.cs

--- StaffRequest.aspx.cs
+++ StaffRequest.aspx.cs
@@ -13,4 +13,21 @@ public partial class StaffRequest : System.Web.UI.Page
     {
 
     }
+    protected void OnRowEvent_SelectRow(object sender, EventArgs e)
+    {
+        //Eddie, check this out and let me know what you think
+        //We need to get the information from the currently selected row and pass it to
+        //The staffrequest table to build it, we can use this as a base and hack it
+        int StaffID1;
+        int StaffID2;
+        int StaffID3;
+        //Get the currently selected row using the SelectedRow property.
+        GridViewRow row = dgCustomer.SelectedRow;
+        // And you respective cell's value
+        StaffID1 = row.Cells[1].Text;
+    }
+    protected void OnButtonClick_btnAddStaff(object sender, EventArgs e)
+    {
+        AddStaffRequest(StaffID1,StaffID2,StaffID3);
+    }
 }
